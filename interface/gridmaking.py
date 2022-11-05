6b9c3e6598b2223146de0c6b2ef9d55de6062615
piche-traful <juliocaesar1@gmail.com>
2022-11-05 14:53:14 +0100

    files for the interface, begin

---
+++ interface/gridmaking.py
@@ -0,0 +1,58 @@
+import pygame
+
+import time
+
+import sys
+
+WIDTH = 800
+
+WIN = pygame.display.set_mode((WIDTH, WIDTH))
+
+""" This is creating the window that we are playing on, it takes a tuple argument which is the dimensions of the window so in this case 800 x 800px
+"""
+
+pygame.display.set_caption("Chess")
+WHITE = (255, 255, 255)
+GREY = (128, 128, 128)
+YELLOW = (204, 204, 0)
+BLUE = (50, 255, 255)
+BLACK = (0, 0, 0)
+
+
+class Node:
+    def __init__(self, row, col, width):
+        self.row = row
+        self.col = col
+        self.x = int(row * width)
+        self.y = int(col * width)
+        self.colour = WHITE
+        self.occupied = None
+
+    def draw(self, WIN):
+        pygame.draw.rect(WIN, self.colour, (self.x, self.y, WIDTH / 8, WIDTH / 8))
+
+    def setup(self, WIN):
+        if starting_order[(self.row, self.col)]:
+            if starting_order[(self.row, self.col)] == None:
+                pass
+            else:
+                WIN.blit(starting_order[(self.row, self.col)], (self.x, self.y))
+
+        """
+        For now it is drawing a rectangle but eventually we are going to need it
+        to use blit to draw the chess pieces instead
+        """
+
+
+def make_grid(rows, width):
+    grid = []
+    gap = WIDTH // rows
+    print(gap)
+    for i in range(rows):
+        grid.append([])
+        for j in range(rows):
+            node = Node(j, i, gap)
+            grid[i].append(node)
+            if (i+j)%2 ==1:
+                grid[i][j].colour = GREY
+    return grid

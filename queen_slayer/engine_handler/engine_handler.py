095a0c65f1c815ebb615d06528ae9f7d13d986c1
FlorianHensel <florianalexanderhensel@gmail.com>
2022-11-14 16:54:34 +0100

    florian | Made Engine Handler work

--- queen_slayer/engine_handler/engine_handler.py
+++ queen_slayer/engine_handler/engine_handler.py
@@ -7,46 +7,64 @@ class EngineHandler():
     Handles the engines' moves.
     '''
 
-    def __init__(self, model):
+    def __init__(self, model_path, weights_path):
+
+        # LOAD MODEL
+        with open(model_path, 'r') as file:
+            model_json = file.read()
+            model = tf.keras.models.model_from_json(model_json)
+
+        # LOAD WEIGHTS
+        model.load_weights(weights_path)
+
+        # COMPILE
+        model.compile(loss='RMSE', optimizer='adam', metrics=['accuracy'])
         self.model = model
 
 
     def predict_move(self, board=chess.Board, depth=1):
         '''
-        Predicts best move.
+        Returns pest move in UCI format.
         '''
 
         # GET LEGAL MOVES (AS UCI)
         legal_moves = [str(i) for i in list(board.legal_moves)]
 
+        # DETERMINE SIDE TO PLAY
+        if board.turn == True:
+            side_to_play = 1
+        else:
+            side_to_play = -1
+
         # PREDICT EVALS FOR EACH LEGAL MOVE
         evaluated_moves = {}
         for move in legal_moves:
 
-            # CREATE COPY OF BOARD AND PUSH MOVE
-            board.push_uci(move)
+            try:
+                # CREATE COPY OF BOARD AND PUSH MOVE
+                board.push_uci(move)
 
-            # CREATE AND ENCODE FEN
-            fen_encoded = Preprocessing.encode_fen(board=board, encoding='square_list')
-            eval = self.model.predict(fen_encoded)
+                # CREATE AND ENCODE FEN
+                fen_encoded = [Preprocessing.encode_fen(board=board, encoding='square_list')]
+                eval = self.model.predict(fen_encoded, verbose=0)[0][0]
 
-            # STORE MOVE AND EVAL
-            evaluated_moves[move] = eval
+                # STORE MOVE AND EVAL
+                evaluated_moves[move] = round(eval, 2)
 
-        # DETERMINE BEST MOVE
+                # RESTORE INITIAL POSITION
+                board.pop()
+
+            except:
+                print(f'{move} | not possible')
+
+        # SORT MOVES BASED ON THEIR EVALS
         ranked_moves = {move: eval for move, eval in sorted(evaluated_moves.items(),
                                                             key=lambda item: item[1])}
 
-        # RETURN BEST MOVE
-        return ranked_moves[0]
+        # RETURN BEST MOVE IF AI IS WHITE
+        if side_to_play == 1:
+            return list(ranked_moves.keys())[-1]
 
-
-# LOAD MODEL
-with open('model/model2.json', 'r') as file:
-    model_json = file.read()
-
-model = tf.keras.models.model_from_json(model_json)
-model.load_weights('model/model2.h5')
-engine_handler = EngineHandler(model)
-board = chess.Board()
-best_move = engine_handler.predict_move(board=board)
+        # RETURN BEST MOVE IF AI IS BLACK
+        else:
+            return list(ranked_moves.keys())[0]

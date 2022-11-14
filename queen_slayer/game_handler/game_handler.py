23fd21ec616d12f7ca370262d39bc97ae2ed4516
FlorianHensel <florianalexanderhensel@gmail.com>
2022-11-14 12:36:22 +0100

    florian | minor change on game_handler

--- queen_slayer/game_handler/game_handler.py
+++ queen_slayer/game_handler/game_handler.py
@@ -6,15 +6,15 @@ class GameHandler(EngineHandler):
     Takes care of game flow.\n
     Communicates with EngineHandler to get AI's move.
     '''
-    def __init__(self, board) -> None:
-        super().__init__(board)
+    def __init__(self, model) -> None:
+        super().__init__(model)
         self.board = chess.Board()
 
 
-    def start_game():
+    def start_game(self):
         pass
 
 
 
 if __name__ == '__main__':
-    GameHandler().start_game()
+    GameHandler(model='model').start_game()

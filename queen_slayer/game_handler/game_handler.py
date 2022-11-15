354103bc26649c57d8d80614fc2fcc5ec871a49f
FlorianHensel <florianalexanderhensel@gmail.com>
2022-11-15 09:09:58 +0100

    florian | adjusted game_handler to unpack eval returned by engine handler

--- queen_slayer/game_handler/game_handler.py
+++ queen_slayer/game_handler/game_handler.py
@@ -10,17 +10,15 @@ print(board)
 while __name__ == '__main__':
 
     print("\n########## USER TURN ##########\n")
-    # user_move = input("Please input a move: ")
-    # print('\n')
-    # board.push_uci(user_move)
-    # print(board)
-    best_move = engine_handler.predict_move(board=board)
-    board.push_uci(best_move)
+    user_move = input("Please input a move: ")
+    print('\n')
+    board.push_uci(user_move)
     print(board)
 
     print("\n########## AI TURN ##########\n")
-    best_move = engine_handler.predict_move(board=board)
+    best_move, eval = engine_handler.predict_move(board=board)
     board.push_uci(best_move)
+    print(f'Best move: {best_move} | Eval: {eval}')
     print(board)
 
 ## DOES ENGINE PREDICT WRONG BEST MOVE??? WHITE INSTEAD BLACK?

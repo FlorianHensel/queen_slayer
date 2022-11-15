dc32c614439decf38c7b16c10e17639ac3b50281
FlorianHensel <florianalexanderhensel@gmail.com>
2022-11-15 18:53:00 +0100

    florian | minor updates

--- queen_slayer/game_handler/game_handler.py
+++ queen_slayer/game_handler/game_handler.py
@@ -7,18 +7,28 @@ board = chess.Board()
 print("\n\n########## GAME START ##########\n")
 print(board)
 
-while __name__ == '__main__':
-
+def user_turn():
     print("\n########## USER TURN ##########\n")
     user_move = input("Please input a move: ")
     print('\n')
     board.push_uci(user_move)
     print(board)
 
+
+def ai_turn():
     print("\n########## AI TURN ##########\n")
     best_move, eval = engine_handler.predict_move(board=board)
     board.push_uci(best_move)
-    print(f'Best move: {best_move} | Eval: {eval}')
+    print(f'Best move: {best_move} | Eval: {round(float(eval), 2)}\n')
     print(board)
 
-## DOES ENGINE PREDICT WRONG BEST MOVE??? WHITE INSTEAD BLACK?
+
+while __name__ == '__main__':
+
+    user_turn()
+    if board.is_checkmate() == True:
+        break
+
+    ai_turn()
+    if board.is_checkmate() == True:
+        break

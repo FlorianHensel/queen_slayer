a452514f8586353500829e917f0ede0205cade3b
FlorianHensel <florianalexanderhensel@gmail.com>
2022-11-24 20:12:33 +0100

    florian | trying different models

--- data_cleaning/qse_model.py
+++ data_cleaning/qse_model.py
@@ -8,6 +8,14 @@ df["eval"] = df["eval"].clip(lower = -15, upper = 15)
 X = df.drop(columns = "eval")
 y = df[["eval"]]
 
+# CHANGE ENCODING
+for i in range(1, 65):
+    X[f'feat_{i}'] = X[f'feat_{i}'].replace([-3, 3], [-4, 4])
+
+# CHANGE ENCODING
+for i in range(1, 65):
+    X[f'feat_{i}'] = X[f'feat_{i}'].replace([-2.9, 2.9], [-3, 3])
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
 # Model definition
@@ -18,9 +26,8 @@ model.add(layers.Dense(300, activation='relu'))
 model.add(layers.Dense(64, activation='relu'))
 model.add(layers.Dense(1, activation='linear'))
 
-
 model.compile(loss='mae', optimizer='adam')
-model.fit(X_train, y_train, batch_size=248, epochs=30,verbose=1)
+model.fit(X_train, y_train, batch_size=248, epochs=15,verbose=1)
 model.evaluate(X_test, y_test, verbose = 1)
 
 # serialize model to JSON
